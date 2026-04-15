d9b9559f95a131d30bc36d8ff4e3952f53e6bdea
Naveen  khan <125086326+Naveen-Khan@users.noreply.github.com>
2026-04-16 00:30:01 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/",   // ✅ ADD THIS LINE
-
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-
   plugins: [
     react(),
-    mode === "development" && componentTagger()
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: [
-      "react",
-      "react-dom",
-      "react/jsx-runtime",
-      "react/jsx-dev-runtime",
-      "@tanstack/react-query",
-      "@tanstack/query-core"
-    ],
   },
+  base: "/portfolio/' // Replace with your GitHub repo name
 }));
